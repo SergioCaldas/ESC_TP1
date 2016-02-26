843fd6dee3638d73ea9f04ec32ff8dfe92387246
Sérgio Caldas <sergiocaldas.91@gmail.com>
2016-02-26 14:29:14 +0000

    SER txt data organized

---
+++ NPB3.3.1/NPB3.3-SER/Results/intel/nodo-641/O3/TXT/intel-O3-nodo_641.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Ficheiro,Tempo,Mop's" >> intel-O3-nodo_641.csv
+
+for ficheiro in *.txt
+do
+  tempo=`grep 'Time in seconds ' $ficheiro`
+  tempo_clean=`echo $tempo | grep -o '[0-9]*\.[0-9]*'`
+  best_time=`echo $tempo_clean | grep -o '[0-9]*\.[0-9]*' | sort |  head -1`
+  mops=`grep 'Mop/s' $ficheiro`
+  mops_clean=`echo $mops | grep -o '[0-9]*\.[0-9]*'`
+  bigger_mops=`echo $mops_clean | grep -o '[0-9]*\.[0-9]*' | sort -k1,1nr | head -1`
+  echo "$ficheiro,$best_time,$bigger_mops" >> intel-O3-nodo_641.csv
+done
