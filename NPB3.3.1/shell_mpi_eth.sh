372d8c1cb7a8e13d972237c0fd5edf6abce3e0c1
SergioCaldas <sergiocaldas.91@gmail.com>
2016-02-28 05:24:51 +0000

    Changed mpi for intel openmpi eth

--- NPB3.3.1/shell_mpi_eth.sh
+++ NPB3.3.1/shell_mpi_eth.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 module purge
-module load gnu/4.9.0
-module load gnu/4.9.3
-module load gnu/openmpi_eth/1.8.4
+module load intel/2013.1.117
+module load intel/openmpi_eth/1.8.2
 ompi_info --param mpi all
 
 cd ~/ESC_TP1/NPB3.3.1/NPB3.3-MPI/
@@ -15,3 +14,4 @@ cd ..
 
 qsub jobs/ivyBridge_MPI_ETH
 
+qsub jobs/nehalem_MPI_ETH
